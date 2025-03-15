d2ae5fa26cebe5cb3b3792e5f63226c62a1e39f9
mete0rfish <sungwon326@naver.com>
2025-03-15 11:04:02 +0900

    init commit

---
+++ src/main/java/com/capstone/dfbf/DfbfApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.dfbf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfbfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfbfApplication.class, args);
+	}
+
+}
