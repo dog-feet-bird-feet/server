e8e26ab2c7f07cd6f8e7aa02acb6b92cf8c401d3
mete0rfish <sungwon326@naver.com>
2025-04-09 14:41:19 +0900

    chore(result): Builder 패턴 적용

--- src/main/java/com/capstone/dfbf/api/result/domain/AnalysisResult.java
+++ src/main/java/com/capstone/dfbf/api/result/domain/AnalysisResult.java
@@ -1,16 +1,17 @@
 package com.capstone.dfbf.api.result.domain;
 
 import com.capstone.dfbf.api.member.Member;
+import com.capstone.dfbf.api.result.dto.AppraisalResponse;
 import com.capstone.dfbf.global.base.BaseEntity;
 import com.github.f4b6a3.ulid.UlidCreator;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Getter
+@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class AnalysisResult extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,4 +25,16 @@ public class AnalysisResult extends BaseEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
     private Member member;
+
+    public AnalysisResult updateWith(final AppraisalResponse response) {
+        return AnalysisResult.builder()
+                .id(this.id)
+                .ulid(this.ulid)
+                .member(this.member)
+                .similarity(response.similarity())
+                .inclination(response.inclination())
+                .pressure(response.pressure())
+                .verificationImgUrl(response.verificationImgUrl())
+                .build();
+    }
 }
