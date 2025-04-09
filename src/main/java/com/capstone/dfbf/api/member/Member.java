36a48d605fccf06c5208ce24ec31e2648fb818cf
mete0rfish <sungwon326@naver.com>
2025-04-09 14:42:37 +0900

    chore(member): Builder 패턴 적용

--- src/main/java/com/capstone/dfbf/api/member/Member.java
+++ src/main/java/com/capstone/dfbf/api/member/Member.java
@@ -5,14 +5,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotNull;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Getter
+@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Member extends BaseEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
