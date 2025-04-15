b9ea5a5f1ca9b3de1a42aa42f8accea5d88c0cd1
mete0rfish <sungwon326@naver.com>
2025-04-15 14:26:42 +0900

    chore: ErrorCode에 항목 추가

--- src/main/java/com/capstone/dfbf/global/exception/error/ErrorCode.java
+++ src/main/java/com/capstone/dfbf/global/exception/error/ErrorCode.java
@@ -8,6 +8,9 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode implements BaseCode{
 
+    // 감정 결과
+    RESULT_NOT_FOUND(HttpStatus.NOT_FOUND, "RST-0000", "결과를 찾을 수 없습니다.", ErrorDisplayType.POPUP),
+
     // 도메인 별 예외는 각 도메인 디렉토리에 BaseCode를 상속한 Code enum을 만들 것
     TMP_ERROR(HttpStatus.BAD_REQUEST, "TMP-0000", "임시 예외", ErrorDisplayType.MODAL),
     BINDING_ERROR(HttpStatus.BAD_REQUEST, "BINDING-0000", "바인딩 에러", ErrorDisplayType.POPUP);
