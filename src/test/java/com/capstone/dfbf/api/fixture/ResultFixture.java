e8c9b41c1783d3b2d639a096539b949d58ee2551
mete0rfish <sungwon326@naver.com>
2025-04-15 15:04:32 +0900

    test(result): AnalysisResult의 테스트 코드 추가

--- src/test/java/com/capstone/dfbf/api/fixture/ResultFixture.java
+++ src/test/java/com/capstone/dfbf/api/fixture/ResultFixture.java
@@ -37,4 +37,10 @@ public class ResultFixture {
                 .verificationImgUrl("https://ggzz.s3.ap-northeast-2.amazonaws.com/verification-img/%E1%84%89%E1%85%A1%E1%86%BC%E1%84%89%E1%85%A6%E1%84%8B%E1%85%B5%E1%84%86%E1%85%B5%E1%84%8C%E1%85%B52.png")
                 .build();
     }
+
+    public static AnalysisResult createNullAnalysisResult() {
+        return AnalysisResult.builder()
+                .id(id)
+                .build();
+    }
 }
