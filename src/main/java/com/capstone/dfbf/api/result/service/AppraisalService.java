6f08b408c97747a733d8952943883f858e44be45
mete0rfish <sungwon326@naver.com>
2025-07-14 14:56:17 +0900

    chore: AI 서버 주소 변경

--- src/main/java/com/capstone/dfbf/api/result/service/AppraisalService.java
+++ src/main/java/com/capstone/dfbf/api/result/service/AppraisalService.java
@@ -27,7 +27,7 @@ import static com.capstone.dfbf.global.exception.error.ErrorCode.MEMBER_NOT_FOUN
 @Service
 public class AppraisalService {
 
-    private final static String fastApiEndpoint = "http://13.124.91.167:8000/api/v1/analyze";
+    private final static String fastApiEndpoint = "http://43.200.169.69:8000/api/v1/analyze";
 
     private final MemberRepository memberRepository;
     private final ResultRepository resultRepository;
